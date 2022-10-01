7e7e8565debaf2e9ed5dd84027c5a6910331674f
Terry <terryakishin0814@gmail.com>
2022-10-01 23:52:49 +0900

    네이버 블로그 API 동작테스트 완

--- src/main/java/com/naverapi/naverapi/component/api/NaverBlogSearchApi.java
+++ src/main/java/com/naverapi/naverapi/component/api/NaverBlogSearchApi.java
@@ -1,7 +1,102 @@
 package com.naverapi.naverapi.component.api;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
+
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.HashMap;
+import java.util.Map;
 
 @Component
 public class NaverBlogSearchApi {
+
+    @Value("${NAVER-CLINET}")
+    private String clientId;
+
+    @Value("${NAVER-KEY}")
+    private String ClientKey;
+
+    public String search( String qeury ) {
+        StringBuilder responseBody = new StringBuilder();
+        // API에서 qeury는 UTF-8 인코딩을 명시하고 있습니다.
+        try {
+            qeury = URLEncoder.encode(qeury, "UTF-8");
+        } catch ( UnsupportedEncodingException e ) {
+            throw new RuntimeException("Query incoding fail", e);
+        }
+        // URL 세팅
+        String apiUrl = "https://openapi.naver.com/v1/search/blog.json?query=" + qeury;
+        // Key Setting
+        Map<String, String> requestHeaders = new HashMap<>();
+        requestHeaders.put("X-Naver-Client-Id", clientId);
+        requestHeaders.put("X-Naver-Client-Secret", ClientKey);
+        // 요청
+        responseBody.append(get(apiUrl,requestHeaders));
+        return responseBody.toString();
+    }
+
+    private String get(String apiUrl, Map<String, String> reqHeaders){
+
+        try {
+            // connection setting
+            HttpURLConnection urlConnection = connect(apiUrl);
+            urlConnection.setReadTimeout(50000);
+            urlConnection.setConnectTimeout(30000);
+            // setting method
+            urlConnection.setRequestMethod("GET");
+            for(Map.Entry<String, String> header : reqHeaders.entrySet()) {
+                urlConnection.setRequestProperty(header.getKey(), header.getValue());
+            }
+            // response
+            int responsCode = urlConnection.getResponseCode();
+            if( responsCode == HttpURLConnection.HTTP_OK ) {
+                return readBody(urlConnection.getInputStream());
+            }
+            return readBody( urlConnection.getErrorStream() );
+
+        } catch (MalformedURLException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    private static HttpURLConnection connect(String apiUrl){
+        try {
+            URL url = new URL(apiUrl);
+            return (HttpURLConnection)url.openConnection();
+        } catch (MalformedURLException e) {
+            throw new RuntimeException("API URL이 잘못되었습니다. : " + apiUrl, e);
+        } catch (IOException e) {
+            throw new RuntimeException("연결이 실패했습니다. : " + apiUrl, e);
+        }
+    }
+
+
+    private static String readBody(InputStream body){
+        InputStreamReader streamReader = new InputStreamReader(body);
+
+
+        try (BufferedReader lineReader = new BufferedReader(streamReader)) {
+            StringBuilder responseBody = new StringBuilder();
+
+
+            String line;
+            while ((line = lineReader.readLine()) != null) {
+                responseBody.append(line);
+            }
+
+
+            return responseBody.toString();
+        } catch (IOException e) {
+            throw new RuntimeException("API 응답을 읽는 데 실패했습니다.", e);
+        }
+    }
+
 }
--- src/main/java/com/naverapi/naverapi/interfaces/contorller/NaverBlogApiController.java
+++ src/main/java/com/naverapi/naverapi/interfaces/contorller/NaverBlogApiController.java
@@ -1,11 +1,36 @@
 package com.naverapi.naverapi.interfaces.contorller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.util.JSONPObject;
+import com.naverapi.naverapi.component.api.NaverBlogSearchApi;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("/blog")
+@RequestMapping("/open")
 public class NaverBlogApiController {
+
+    @Autowired
+    NaverBlogSearchApi naverBlogSearchApi = new NaverBlogSearchApi();
+
+    @GetMapping("/test")
+    public String test()
+    {
+        return "hello";
+    }
+
+    @ResponseBody
+    @GetMapping("/naver/blog")
+    public ResponseEntity<JSONObject> getPlace(@RequestParam(value = "query") String query ) throws Exception {
+        JSONParser parser = new JSONParser();
+        Object obj = parser.parse( naverBlogSearchApi.search(query));
+        JSONObject jsonObj = (JSONObject) obj;
+
+        return ResponseEntity.ok(jsonObj);
+    }
 }
