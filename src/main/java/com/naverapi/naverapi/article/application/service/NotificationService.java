8f89cca50a40a80064c9e6ab4597b178610d2bce
Terry <terryakishin0814@gmail.com>
2022-10-05 19:38:20 +0900

    google mail 발송 서비스 로직 작성

--- src/main/java/com/naverapi/naverapi/article/application/service/NotificationService.java
+++ src/main/java/com/naverapi/naverapi/article/application/service/NotificationService.java
@@ -1,11 +1,39 @@
 package com.naverapi.naverapi.article.application.service;
 
+import com.naverapi.naverapi.article.component.email.MailSenderByGoogle;
+import com.naverapi.naverapi.article.domain.email.Email;
+import com.naverapi.naverapi.user.domain.User;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
 
 @Service
 @Slf4j
 @RequiredArgsConstructor
 public class NotificationService {
+    private static final String EXAMPLE_LINK_TEMPLATE = "mail/mail";
+    private final TemplateEngine templateEngine;
+    private final MailSenderByGoogle mailSenderByGoogle;
+
+    public void sendNotificationByEmail( User user ) {
+
+        Context context = getContext( user.getName() );
+        String message = templateEngine.process(EXAMPLE_LINK_TEMPLATE, context);
+
+        mailSenderByGoogle.sendMailHtml(   Email.builder()
+                                        .address(user.getEmail())
+                                        .title("test")
+                                        .message(message)
+                                        .build()   );
+    }
+
+    private Context getContext( String name ) {
+        Context context = new Context();
+        context.setVariable("name", name);
+        context.setVariable("message", "블로그 추천 콘텐츠입니다.");
+        return context;
+    }
 }
--- src/main/java/com/naverapi/naverapi/article/component/email/MailSenderByGoogle.java
+++ src/main/java/com/naverapi/naverapi/article/component/email/MailSenderByGoogle.java
@@ -14,7 +14,7 @@ import javax.mail.internet.MimeMessage;
 @Component
 @Slf4j
 @RequiredArgsConstructor
-public class MailSender {
+public class MailSenderByGoogle {
     @Autowired
     private JavaMailSender mailSender;
 
@@ -22,13 +22,15 @@ public class MailSender {
         MimeMessage mimeMessage = mailSender.createMimeMessage();
 
         try {
+
             MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage, false, "UTF-8");
-            mimeMessageHelper.setTo( email.getAddress());
-            mimeMessageHelper.setSubject(email.getTitle());
+            mimeMessageHelper.setTo( email.getAddress() );
+            mimeMessageHelper.setSubject(email.getTitle() );
             mimeMessageHelper.setText( email.getMessage(), true);
 
-            mailSender.send(mimeMessage);
+            mailSender.send( mimeMessage );
             log.info("msg send : " + mimeMessage.getMessageID());
+
         } catch ( MessagingException e ) {
             log.info(e.getMessage());
         }
