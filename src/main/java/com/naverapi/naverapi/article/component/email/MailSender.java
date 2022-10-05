86fa87bd03b7c4794dd86edb6e3937a75a72c93f
Terry <terryakishin0814@gmail.com>
2022-10-05 17:54:39 +0900

    MailSender Component 작성

--- src/main/java/com/naverapi/naverapi/article/component/email/MailSender.java
+++ src/main/java/com/naverapi/naverapi/article/component/email/MailSender.java
@@ -1,5 +1,6 @@
 package com.naverapi.naverapi.article.component.email;
 
+import com.naverapi.naverapi.article.domain.email.Email;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,6 +18,19 @@ public class MailSender {
     @Autowired
     private JavaMailSender mailSender;
 
-    public void sendMailHtml() {
+    public void sendMailHtml(Email email) {
+        MimeMessage mimeMessage = mailSender.createMimeMessage();
+
+        try {
+            MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage, false, "UTF-8");
+            mimeMessageHelper.setTo( email.getAddress());
+            mimeMessageHelper.setSubject(email.getTitle());
+            mimeMessageHelper.setText( email.getMessage(), true);
+
+            mailSender.send(mimeMessage);
+            log.info("msg send : " + mimeMessage.getMessageID());
+        } catch ( MessagingException e ) {
+            log.info(e.getMessage());
+        }
     }
 }
