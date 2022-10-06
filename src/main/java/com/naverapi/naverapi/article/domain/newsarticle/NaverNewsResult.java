1efaaf503c253a9abf411efed054b10ccb2d73df
Terry <terryakishin0814@gmail.com>
2022-10-06 21:59:48 +0900

    New 조회 결과를 저장하기위한 Dto

--- src/main/java/com/naverapi/naverapi/article/domain/newsarticle/NaverNewsResult.java
+++ src/main/java/com/naverapi/naverapi/article/domain/newsarticle/NaverNewsResult.java
@@ -30,14 +30,18 @@ public class NaverNewsResult {
     @Column(length = 1024, nullable = false)
     private String pubDate;
 
+    @Column(length = 1024, nullable = false)
+    private String md5HahCode;
+
     @Builder
     public NaverNewsResult(String title, String originallink, String link,
-                           String description, String pubDate) {
+                           String description, String pubDate, String md5HahCode) {
         this.title = title;
         this.originallink = originallink;
         this.link = link;
         this.description = description;
         this.pubDate = pubDate;
+        this.md5HahCode = md5HahCode;
     }
 
 }
