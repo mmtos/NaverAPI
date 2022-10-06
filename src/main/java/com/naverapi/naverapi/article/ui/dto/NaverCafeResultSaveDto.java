48bfdba161c2938e78dfed10dfa94b6d9027ef65
Terry <terryakishin0814@gmail.com>
2022-10-06 23:30:01 +0900

    import 정리

--- src/main/java/com/naverapi/naverapi/article/ui/dto/NaverCafeResultSaveDto.java
+++ src/main/java/com/naverapi/naverapi/article/ui/dto/NaverCafeResultSaveDto.java
@@ -6,8 +6,6 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.Column;
-
 import static com.naverapi.naverapi.article.component.util.Md5.makeMd5;
 
 @Getter
