0c121c8ff67df6fa54f51a2c5d87d93fdb0a36ee
Terry <terryakishin0814@gmail.com>
2022-10-06 17:19:06 +0900

    import 정리

--- src/main/java/com/naverapi/naverapi/article/domain/apirequest/ApiReponseRepository.java
+++ src/main/java/com/naverapi/naverapi/article/domain/apirequest/ApiReponseRepository.java
@@ -1,7 +1,6 @@
 package com.naverapi.naverapi.article.domain.apirequest;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 public interface ApiReponseRepository extends JpaRepository<ApiReponse, Long> {
 }
--- src/main/java/com/naverapi/naverapi/article/domain/blogarticle/NaverBlogResultRepository.java
+++ src/main/java/com/naverapi/naverapi/article/domain/blogarticle/NaverBlogResultRepository.java
@@ -1,8 +1,6 @@
 package com.naverapi.naverapi.article.domain.blogarticle;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 public interface NaverBlogResultRepository extends JpaRepository<NaverBlogResult, Long> {
-    Long countBy();
 }
