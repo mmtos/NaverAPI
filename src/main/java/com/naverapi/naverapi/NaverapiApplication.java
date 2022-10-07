f0e17821889c5e3d30c4d9eb1147802c5d66b6a3
Terry <terryakishin0814@gmail.com>
2022-10-08 00:19:34 +0900

    비동기 처리를 위한 thread 설정을 추가

--- src/main/java/com/naverapi/naverapi/NaverapiApplication.java
+++ src/main/java/com/naverapi/naverapi/NaverapiApplication.java
@@ -3,13 +3,11 @@ package com.naverapi.naverapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
 @EnableJpaAuditing
 @SpringBootApplication
-@EnableAsync
 public class NaverapiApplication {
 
 	public static void main(String[] args) {
