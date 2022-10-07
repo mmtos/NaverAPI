423525dc1a52db7de0d5689aa988a2df1a7e27f5
Terry <terryakishin0814@gmail.com>
2022-10-08 02:30:48 +0900

    User 조회 테스트

--- src/main/java/com/naverapi/naverapi/article/component/schedule/NaverApiScheduler.java
+++ src/main/java/com/naverapi/naverapi/article/component/schedule/NaverApiScheduler.java
@@ -9,9 +9,11 @@ import com.naverapi.naverapi.article.domain.email.EmailEvent;
 import com.naverapi.naverapi.article.domain.email.EmailRepository;
 import com.naverapi.naverapi.article.domain.email.EmailType;
 import com.naverapi.naverapi.article.domain.event.EmailEventQueue;
+import com.naverapi.naverapi.user.application.service.UserService;
 import com.naverapi.naverapi.user.domain.Role;
 import com.naverapi.naverapi.user.domain.User;
 import com.naverapi.naverapi.user.domain.UserRepository;
+import com.naverapi.naverapi.user.ui.contorller.dto.UserResponseDto;
 import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -20,6 +22,8 @@ import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Slf4j
 @Component
@@ -31,15 +35,22 @@ public class NaverApiScheduler {
 
     @Autowired
     private EventPublisher publisher;
-
-    private final UserRepository userRepository;
     private final EmailEventQueue eventQueue;
     private final EmailRepository emailRepository;
     private final NotificationService notificationService;
 
-    @Scheduled( cron = "0 */1 * * * *")
-    public void naverApiTest() throws InterruptedException {
+    private final UserService userService;
 
+    @Scheduled( cron = "0 */1 * * * *")
+    public void getAllUserTest() throws InterruptedException {
+        List<UserResponseDto> userResponseDtoList = userService.findAllDesc();
+
+        for ( UserResponseDto dto : userResponseDtoList) {
+            log.info(String.valueOf(dto.getId()) + dto.getName() + dto.getEmail());
+            for(Email email : dto.getEmailList() ) {
+                log.info(email.getTitle());
+            }
+        }
     }
 
     @Scheduled(fixedRate = 100)
