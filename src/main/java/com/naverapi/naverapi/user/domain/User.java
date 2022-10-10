c7685d6191d212a31bd4b2fb878c2234fee11335
Terry <terryakishin0814@gmail.com>
2022-10-10 20:59:57 +0900

    import 정리

--- src/main/java/com/naverapi/naverapi/user/domain/User.java
+++ src/main/java/com/naverapi/naverapi/user/domain/User.java
@@ -1,17 +1,11 @@
 package com.naverapi.naverapi.user.domain;
 
-import com.naverapi.naverapi.article.domain.email.Email;
 import com.naverapi.naverapi.common.BaseTimeEntity;
 
 import javax.persistence.*;
-
-import com.naverapi.naverapi.keyword.domain.KeyWord;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @Getter
 @NoArgsConstructor
